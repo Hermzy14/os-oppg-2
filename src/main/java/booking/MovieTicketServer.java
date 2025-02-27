b0578fb2b6a52c7d11744c9b009cd62050fcd9ff
Herman Lundby-Holen <hermanluho@gmail.com>
2025-02-27 11:07:21 +0100

    Added tickets left print.

--- src/main/java/booking/MovieTicketServer.java
+++ src/main/java/booking/MovieTicketServer.java
@@ -4,36 +4,33 @@ package booking;
  * This class is responsible for booking tickets for a movie
  */
 public class MovieTicketServer {
-private String movieName;
-private int availableTickets; //change this to volatile for tasks
+  private String movieName;
+  private volatile int availableTickets; //change this to volatile for tasks
 
   /**
    * Constructor
-   * @param movieName
-   * @param availableTickets
    *
-   * Creates a move ticket server with a given movie name and available tickets
+   * @param movieName
+   * @param availableTickets Creates a move ticket server with a given movie name and available tickets
    */
   public MovieTicketServer(String movieName, int availableTickets) {
     this.movieName = movieName;
     this.availableTickets = availableTickets;
-}
+  }
 
   /**
    * @param name
-   * @param tickets
-   *
-   * Books tickets for a movie
+   * @param tickets Books tickets for a movie
    */
   public synchronized void bookTicket(String name, int tickets) {
-  if (availableTickets >= tickets) {
-    System.out.println(name + " booked " + tickets + " tickets for " + movieName);
-    availableTickets -= tickets;
-  } else {
-    System.out.println(
-        "Sorry, " + name + " we only have " + availableTickets + " tickets left for " + movieName);
+    if (availableTickets >= tickets) {
+      System.out.println(name + " booked " + tickets + " tickets for " + movieName);
+      availableTickets -= tickets;
+      System.out.println("Tickets left: " + availableTickets);
+    } else {
+      System.out.println(
+          "Sorry, " + name + " we only have " + availableTickets + " tickets left for " +
+              movieName);
+    }
   }
-}
-
-
 }
